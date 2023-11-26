b6b83faff081e10b5329d755798c027c1d7673e6
Alex Alexis <1334629+aalexis8@users.noreply.github.com>
2023-11-25 19:16:50 -0500

    counting words, printing count to the console

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ with open('books/frankenstein.txt', 'r') as f:
     #text = f.read()
     file_contents = f.read()
     print(file_contents)
+    words_count = len(file_contents.split())
+    print(words_count)
+
