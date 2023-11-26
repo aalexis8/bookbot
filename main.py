63b1047002adc09b2a2585add4cd0d6b654d19f3
Alex Alexis <1334629+aalexis8@users.noreply.github.com>
2023-11-26 09:54:52 -0500

    add function to count characters, no dups, seems to work

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ with open('books/frankenstein.txt', 'r') as f:
     words_count = len(file_contents.split())
     print(words_count)
 
+def count_characters(file_contents):
+    file_contents = file_contents.lower()
+    counts = {}
+    for character in file_contents:
+        if character.isalpha():
+            if character in counts:
+                counts[character] += 1
+            else:
+                counts[character] = 1
+    return counts
+
+results = count_characters(file_contents)
+print(results)
